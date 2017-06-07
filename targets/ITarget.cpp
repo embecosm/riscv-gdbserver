1f024861ed1028b9f36a2944a1f4265e593f4d6e
Jeremy Bennett <jeremy.bennett@embecosm.com>
2017-06-07 19:54:17 +0100

    Abstract the target interface
    	Infrastructure for an abstracted target interface class, ITarget.
    	All targets should implement the interface class ITarget, declared
    	in the "targets" subdirectory.  Each target should be in its own
    	subdirectory of the "targets" directory.  There is also a "common"
    	subdirectory for code common to all targets.
    
    	The result is the libtargets library, which can be linked into the
    	server code, which can instantiate the target of choice.  The
    	current framework has placeholders for the PicoRV32 and RI5CY
    	verilator models.
    
    ChangeLog:
    
    	* Makefile.am: Add new subdirectories and have automake include
    	the m4 directory.
    	* Makefile.in: Regenerated.
    	* aclocal.m4: Regenerated for libtool.
    	* compile: Generated.
    	* config.guess: Generated.
    	* config.h.in: Regenerated.
    	* config.sub: Generated.
    	* configure: Regenerated.
    	* configure.ac: Now use libtool and for now comment out the
    	verilator directory.
    	* ltmain.sh: Generated.
    	* m4/libtool.m4: Generated.
    	* m4/ltoptions.m4: Generated.
    	* m4/ltsugar.m4: Generated.
    	* m4/ltversion.m4: Generated.
    	* m4/lt~obsolete.m4: Generated.
    	* server/Makefile.in: Regenerated.
    	* targets/ITarget.cpp: Created
    	* targets/ITarget.h: Created
    	* targets/Makefile.am: Created
    	* targets/Makefile.in: Generated.
    	* targets/common/Makefile.am: Created
    	* targets/common/Makefile.in: Generated.
    	* targets/common/SyscallInfo.cpp: Created
    	* targets/common/SyscallInfo.h: Created
    	* targets/picorv32/Makefile.am: Created
    	* targets/picorv32/Makefile.in: Generated.
    	* targets/ri5cy/Makefile.am: Created
    	* targets/ri5cy/Makefile.in: Generated.

---
+++ targets/ITarget.cpp
@@ -0,0 +1,114 @@
+// Generic GDB RSP server interface: definition.
+
+// Copyright (C) 2008-2017  Embecosm Limited <www.embecosm.com>
+
+// Contributor Jeremy Bennett <jeremy.bennett@embecosm.com>
+
+// This file is part of the RISC-V GDB server
+
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.
+
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+// Even though ITarget is an abstract class, it requires implementation of the
+// stream operators to allow its public scoped enumerations to be output.  It
+// also requires implementation of the concrete SyscallInfo class.
+
+#include "ITarget.h"
+
+
+//! Output operator for ResumeType enumeration
+
+//! @param[in] s  The stream to output to.
+//! @param[in] p  The ResumeType value to output.
+//! @return  The stream with the item appended.
+
+std::ostream &
+operator<< (std::ostream & s,
+	    ITarget::ResumeType  p)
+{
+  const char * name;
+
+  switch (p)
+    {
+    case ITarget::ResumeType::STEP:     name = "step";     break;
+    case ITarget::ResumeType::CONTINUE: name = "continue"; break;
+    case ITarget::ResumeType::STOP:     name = "stop";     break;
+    default:                            name = "unknown";  break;
+    }
+
+  return  s << name;
+
+}	// operator<< ()
+
+
+//! Output operator for ResumeRes enumeration
+
+//! @param[in] s  The stream to output to.
+//! @param[in] p  The ResumeRes value to output.
+//! @return  The stream with the item appended.
+
+std::ostream &
+operator<< (std::ostream & s,
+	    ITarget::ResumeRes  p)
+{
+  const char * name;
+
+  switch (p)
+    {
+    case ITarget::ResumeRes::NONE:        name = "none";        break;
+    case ITarget::ResumeRes::SUCCESS:     name = "success";     break;
+    case ITarget::ResumeRes::FAILURE:     name = "failure";     break;
+    case ITarget::ResumeRes::INTERRUPTED: name = "interrupted"; break;
+    case ITarget::ResumeRes::TIMEOUT:     name = "timeout";     break;
+    case ITarget::ResumeRes::SYSCALL:     name = "syscall";     break;
+    default:                              name = "unknown";     break;
+    }
+
+  return  s << name;
+
+}	// operator<< ()
+
+
+//! Output operator for MatchType enumeration
+
+//! @param[in] s  The stream to output to.
+//! @param[in] p  The MatchType value to output.
+//! @return  The stream with the item appended.
+
+std::ostream &
+operator<< (std::ostream & s,
+	    ITarget::MatchType  p)
+{
+  const char * name;
+
+  switch (p)
+    {
+    case ITarget::MatchType::BREAK:        name = "breakpoint";          break;
+    case ITarget::MatchType::BREAK_HW:     name = "hardware breakpoint"; break;
+    case ITarget::MatchType::WATCH_WRITE:  name = "write watchpoint";    break;
+    case ITarget::MatchType::WATCH_READ:   name = "read watchpoint";     break;
+    case ITarget::MatchType::WATCH_ACCESS: name = "access watchpoint";   break;
+    default:                               name = "unknown";             break;
+    }
+
+  return  s << name;
+
+}	// operator<< ()
+
+
+// Local Variables:
+// mode: C++
+// c-file-style: "gnu"
+// show-trailing-whitespace: t
+// End:
---
+++ targets/ITarget.h
@@ -0,0 +1,148 @@
+// Generic GDB RSP server interface: declaration
+
+// Copyright (C) 2008-2017  Embecosm Limited <www.embecosm.com>
+
+// Contributor Jeremy Bennett <jeremy.bennett@embecosm.com>
+
+// This file is part of the RISC-V GDB server
+
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.
+
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef ITARGET__H
+#define ITARGET_H
+
+#include <chrono>
+#include <cstddef>
+#include <cstdint>
+#include <iostream>
+
+#include <SyscallInfo.h>
+
+
+//! Generic interface class for GDB RSP server targets.
+
+//! Being an interface class, it has no state.  But it does have some
+//! behavior, in the form of stream handling methods.
+
+class ITarget
+{
+ public:
+
+  //! Type of resume to use.
+
+  //! STOP is to request the target to stop.  This is for use after a previous
+  //! call has indicated a timeout, for targets where stopping is very
+  //! expensive.
+
+  enum class ResumeType : int {
+    STEP     = 0,
+    CONTINUE = 1,
+    STOP     = 2
+  };
+
+  //! Result of execution methods.
+
+  enum class ResumeRes : uint32_t {
+    NONE        = 0,		//!< Place holder when we don't want to stop.
+    SUCCESS     = 1,            //!< Execution was successful.
+    FAILURE     = 2,            //!< Execution failed.
+    INTERRUPTED = 3,            //!< Execution interrupted (e.g. breakpoint).
+    TIMEOUT     = 4,		//!< Execution hit time limit.
+    SYSCALL     = 5,		//!< Target needs some host I/O.
+  };
+
+  //! Type of matchpoint, with mapping to RSP Z/z packet values.
+
+  enum class MatchType : int {
+    BREAK        = 0,		//!< Software/memory breakpoint
+    BREAK_HW     = 1,		//!< Hardware breakpoint
+    WATCH_WRITE  = 2,		//!< Write watchpoint
+    WATCH_READ   = 3,		//!< Read watchpoint
+    WATCH_ACCESS = 4		//!< Access watchpoint
+    };
+
+  // Virtual destructor for defined behavior
+
+  virtual ~ITarget (void) {};
+
+  virtual ResumeRes  resume (ResumeType step,
+			     SyscallInfo *syscall_info = nullptr) = 0;
+  virtual ResumeRes  resume (ResumeType step,
+                             std::chrono::duration <double>  timeout,
+                             SyscallInfo *syscall_info = nullptr) = 0;
+
+  virtual ResumeRes  terminate (void) = 0;
+  virtual ResumeRes  reset (void) = 0;
+
+  virtual uint64_t  getCycleCount (void) const = 0;
+  virtual uint64_t  getInstrCount (void) const = 0;
+
+  // Read contents of a target register.
+
+  virtual std::size_t  readRegister (const int  reg,
+				      uint32_t & value) const = 0;
+
+  // Write data to a target register.
+
+  virtual std::size_t  writeRegister (const int  reg,
+				      const uint32_t  value) = 0;
+
+  // Read data from memory.
+
+  virtual std::size_t  read (const uint32_t & addr,
+			     uint8_t * buffer,
+			     const std::size_t  size) const = 0;
+
+  // Write data to memory.
+
+  virtual std::size_t  write (const uint32_t & addr,
+			      const uint8_t * buffer,
+			      const std::size_t  size) = 0;
+
+  // Insert and remove a matchpoint (breakpoint or watchpoint) at the given
+  // address.  Return value indicates whether the operation was successful.
+
+  virtual bool  insertMatchpoint (const uint32_t & addr,
+				  const MatchType  matchType) = 0;
+  virtual bool  removeMatchpoint (const uint32_t & addr,
+				  const MatchType  matchType) = 0;
+
+  // Generic pass through of command
+
+  virtual bool command (const std::string  cmd,
+			std::ostream & stream) = 0;
+};
+
+
+// Output operators for scoped enumerations
+
+std::ostream &
+operator<< (std::ostream & s,
+	    ITarget::ResumeType  p);
+std::ostream &
+operator<< (std::ostream & s,
+	    ITarget::ResumeRes  p);
+std::ostream &
+operator<< (std::ostream & s,
+	    ITarget::MatchType  p);
+
+
+#endif	// ITARGET_H
+
+
+// Local Variables:
+// mode: C++
+// c-file-style: "gnu"
+// show-trailing-whitespace: t
+// End:
