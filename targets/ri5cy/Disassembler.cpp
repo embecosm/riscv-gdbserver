e52af2c7c89823ecc88b9c6154acce34ca1de6fe
Jeremy Bennett <jeremy.bennett@embecosm.com>
2017-08-07 19:46:23 +0100

    Get disassembly to build.
    	Fix the automake files for using disassembly.  The issue was that
    	binutils dis-asm.h did not have print_insn_riscv marked as for
    	external use.  Once that is resolved these changes allow the
    	server to build.
    
    ChangeLog:
    
    	* Makefile.in: Regenerated.
    	* configure: Regenerated.
    	* configure.ac: Cleanly define BINUTILS_INCDIR and BINUTILS_LIBDIR
    	* server/Makefile.am: No need for libibert in LDADD
    	* server/Makefile.in: Regenerated.
    	* targets/Makefile.in: Regenerated.
    	* targets/common/Makefile.in: Regenerated.
    	* targets/ri5cy/Disassembler.cpp: Add license header, use C++
    	style C includes.
    	* targets/ri5cy/Disassembler.h (class Disassembler): Add license
    	header, use C++ style C includes
    	* targets/ri5cy/Makefile.am: Simplify includes dir for binutils
    	utils.
    	* targets/ri5cy/Makefile.in: Regenerated.

--- targets/ri5cy/Disassembler.cpp
+++ targets/ri5cy/Disassembler.cpp
@@ -1,10 +1,32 @@
+// GDB RSP server RI5CY CPU model: definition
+
+// Copyright (C) 2017  Embecosm Limited <info@embecosm.com>
+
+// Contributor Ian Bolton <ian.bolton@embecosm.com>
+
+// This file is part of the RISC-V GDB server
+
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+// License for more details.
+
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "Disassembler.h"
 #include "config.h"
-#include <string.h>
+#include <cstring>
 #include "dis-asm.h"
-#include "disassemble.h"
+//#include "disassemble.h"
 #include "bfd.h"
-#include <stdint.h>
+#include <cstdint>
+#include <cstdio>
 
 
 Disassembler::Disassembler ()
@@ -16,6 +38,7 @@ Disassembler::Disassembler ()
   disasm_info.buffer_vma = (bfd_vma) vals;
   disasm_info.buffer_length = 4;    
 }
+
 
 void Disassembler::disassemble_riscv (uint32_t insn)
 {
--- targets/ri5cy/Disassembler.h
+++ targets/ri5cy/Disassembler.h
@@ -1,20 +1,48 @@
+// GDB RSP server RI5CY CPU model: definition
+
+// Copyright (C) 2017  Embecosm Limited <info@embecosm.com>
+
+// Contributor Ian Bolton <ian.bolton@embecosm.com>
+
+// This file is part of the RISC-V GDB server
+
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+// License for more details.
+
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+#ifndef DISASSEMBLER_H
+#define DISASSEMBLER_H
+
 #include "config.h"
-#include <string.h>
-#include "dis-asm.h"
-#include "disassemble.h"
+#include <cstring>
 #include "bfd.h"
+#include "dis-asm.h"
+//#include "disassemble.h"
 #include <stdint.h>
+
 
 class Disassembler
 {
 
-  public:
+public:
   Disassembler ();
   ~Disassembler () { }
-  
+
   void disassemble_riscv (uint32_t insn);
 
-  private:
+private:
   bfd_byte vals[4];
   struct disassemble_info disasm_info;
 };
+
+#endif	// DISASSEMBLER_H
